eb7cb432e32eb34701f25022891ad9791576fad4
csw <806695981@qq.com>
2017-12-24 17:59:12 -0800

    add simple DES

--- modsymmetric/simple-des.cpp
+++ modsymmetric/simple-des.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class SDES {
 public:
-    SDES();
+    SDES(string splain, string skey);
     void setKey(string s);
     void setPlainText(string s);
     void fk(int key, int& left, int right);
@@ -18,7 +18,7 @@ private:
     int key, key1, key2;
     int plainText;
     int cipherText;
-    int deciperText;
+    int decipherText;
     int stringToInt(string s);
     static const int P10[10];
     static const int P8[8];
@@ -41,13 +41,21 @@ const int SDES::P4[4] = {2, 4, 3, 1};
 const int SDES::S0[4][4] = {{1, 0, 3, 2}, {3, 2, 1, 0}, {0, 2, 1, 3}, {3, 1, 3, 2}};
 const int SDES::S1[4][4] = {{0, 1, 2, 3}, {2, 0, 1, 3}, {3, 0, 1, 0}, {2, 1, 0, 3}};
 
+SDES::SDES(string splain, string skey) {
+    setPlainText(splain);
+    setKey(skey);
+    getKey();
+}
+
 void SDES::setKey(string s) {
     key = stringToInt(s);
 }
-
-int SDES::stringToInt(s) {
+void SDES::setPlainText(string s) {
+    plainText = stringToInt(s);
+}
+int SDES::stringToInt(string s) {
     int key = 0;
-    for (i = 0; i < s.length(); i++) {
+    for (int i = 0; i < s.length(); i++) {
         key = key * 2 + s[i] - '0';
     }
     return key;
@@ -62,3 +70,84 @@ void SDES::getKey() {
     key2 = permutations(tempKey, P8, 10, 8);
 }
 
+
+
+int SDES::permutations(int num, const int p[], int pmax, int n) {
+    int temp = 0;
+    for (int i = 0; i < n; i++) {
+        temp <<= 1;
+        temp |= (num >> (pmax - p[i])) & 1;
+    }
+    return temp;
+}
+
+int SDES::shiftLS(int num) {
+    int temp, L, R;
+    L = (num >> 5) & 0x1F;
+    R = num & 0x1F;
+    L = ((L & 0xF) << 1) | ((L & 0x10) >> 4);
+    R = ((R & 0xF) << 1) | ((R & 0x10) >> 4);
+    temp = (L << 5) | R;
+    return temp;
+}
+
+void SDES::encryption() {
+    cipherText = permutations(plainText, IP, 8, 8);
+    int temp, R, L;
+    R = cipherText & 0xF0;
+    L = ((cipherText & 0xF0) >> 4);
+    fk(key1, L, R);
+    temp = L, L = R, R = temp;
+    fk(key2, L, R);
+    temp = (L << 4) | R;
+    cipherText = permutations(temp, IPI, 8, 8);
+}
+
+void SDES::fk(int key, int& left, int right) {
+    int L = left, R = right, temp, tempL, tempR;
+    temp = ((permutations(R, EP1, 4, 4)) << 4) | (permutations(R, EP2, 4, 4));
+    temp = temp ^ key;
+    tempR = temp & 0xF;
+    tempL = ((temp & 0xF0) >> 4);
+    tempL = S0[((tempL & 0x8) >> 2) | (tempL & 1)][(tempL >> 1) & 0x3];
+    tempR = S1[((tempR & 0x8) >> 2) | (tempR & 1)][(tempR >> 1) & 0x3];
+    temp = (tempL << 2) | tempR;
+    temp = permutations(temp, P4, 4, 4);
+    left = L ^ temp;    
+}
+
+void SDES::decryption() {
+    decipherText = permutations(cipherText, IP, 8, 8);
+    int temp, R, L;
+    R = decipherText & 0xF;
+    L = (decipherText & 0xF) >> 4;
+    fk(key2, L, R);
+    temp = L, L = R, R = temp;
+    fk(key1, L, R);
+    temp = (L << 4) | R;
+    decipherText = permutations(temp, IPI, 8, 8);
+}
+
+void SDES::showResult() {
+    cout << "key :" << key << endl;
+    cout << "key1 :" << key1 << endl;
+    cout << "key2 :" << key2 << endl;
+    cout << "plainText :" << plainText << endl;
+    cout << "cipherText :" << cipherText << endl;
+    cout << "decipherText :" << decipherText << endl;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2 && argc != 3) {
+        cout << "usage: ./a.out plaintext [key]" << endl;
+        return 0;
+    }
+    string tkey, tplaintext;
+    tplaintext = argv[1];
+    tkey = (argc == 3 ? argv[2] : "1010110101");
+    SDES sdes(tplaintext ,tkey);
+    sdes.encryption();
+    sdes.decryption();
+    sdes.showResult();
+    return 0;
+}
